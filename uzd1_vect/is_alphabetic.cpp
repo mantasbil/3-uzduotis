7e60d98892f4d03106b970445d08aafe836b83f3
mantasbil <83337672+mantasbil@users.noreply.github.com>
2021-11-26 14:14:07 +0200

    Delete uzd1_vect directory

--- uzd1_vect/is_alphabetic.cpp
+++
@@ -1,6 +0,0 @@
-#include "is_alphabetic.h"
-
-bool is_alphabetic(std::string vardas)
-{
-    return std::all_of(vardas.begin(), vardas.end(), isalpha);
-}
--- uzd1_vect/is_alphabetic.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-#include <algorithm>
-#include "studentas.h"
-
-bool is_alphabetic(std::string vardas);
