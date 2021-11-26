e3f1cde74e2910329e9e9c7c136fcac8bf6b91ef
mantasbil <83337672+mantasbil@users.noreply.github.com>
2021-11-26 14:58:03 +0200

    Delete student_print.h

--- student_print.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "studentas.h"
-#include "compare_alphabet.h"
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-
-void student_print(std::vector <studentas> grupe, std::string type);
